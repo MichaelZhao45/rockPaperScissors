ba585e77ee3008ff9353e7e47d98119ff911a9ac
Michael <Michaelzhao4567@gmail.com>
2023-11-01 17:23:17 -0400

    rps user interface working, just need to design

--- rps.js
+++ rps.js
@@ -1,5 +1,8 @@
 /* Rock Paper Scissors Java Script Oden Project*/
 
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {                  // computer selection
     let num = Math.floor(Math.random() * 3);
 
@@ -14,73 +17,71 @@ function getComputerChoice() {                  // computer selection
     }
 }
 
+function updateScore() {
+    const scorePlayerDiv = document.getElementById("player");
+    const scoreComputerDiv = document.getElementById("computer");
+    scorePlayerDiv.textContent = playerScore;
+    scoreComputerDiv.textContent = computerScore;
 
+}
 
-function playRound(playerSelection, computerSelection) {    // play single round of game
+function getWinner() {
+    const winnerDiv = document.getElementById("winner");
+    if (playerScore === 5) {
+        winnerDiv.textContent = "Player Wins!";
+    }
+    else if (computerScore === 5) {
+        winnerDiv.textContent = "Computer Wins";
+    }
     
+}
+
+
+function playRound(playerSelection) {    // play single round of game
+    
+    const computerSelection = getComputerChoice();      // get computer Selection
+    let result;
+    let computerWin = false;
+    let playerWin = false;
+    const computerDiv = document.getElementById("computerChoice");
+    computerDiv.textContent = computerSelection;
+
     if (playerSelection === computerSelection) {
-        return 2;
+        result = "Tie!";
     }
     else if (playerSelection === "rock" && computerSelection === "paper") { 
-        return 1;
+        result = "Computer Wins";
+        computerWin = true;
     }
     else if (playerSelection === "scissors" && computerSelection === "rock") {
-            return 1;
-        }
+        result = "Computer Wins";
+        computerWin = true;
+    }
     
     else if (playerSelection === "paper" && computerSelection === "scissors") {
-            return 1;
+        result = "Computer Wins";
+        computerWin = true;
     }
     else {
-        return 0;
+        result = "Player Wins";
+        playerWin = true;
     }
 
+    if (playerWin) {
+        playerScore += 1;
+        console.log("Player won")
+    }
+    if (computerWin) {
+        computerScore += 1;
+        console.log("Computer won")
+    }
+
+    const resultDiv = document.getElementById("results");
+    resultDiv.textContent = result;
+
+    updateScore();
+    getWinner();
+    
+
 }
 
-
-function game() {               // game function, plays game best of 5               
-
-    let computerWins = 0;
-    let playerWins = 0;
-    
-    const buttons = document.querySelectorAll('button');
-
-    buttons.forEach((button) => {
-        button.addEventListener('click', () => {
-            const playerSelection = button.id;
-            const computerSelection = getComputerChoice();
-            const winner = playRound(playerSelection,computerSelection);
-
-        });
-        console.log("Computer Plays...", computerSelection);
-        console.log("Player Plays...", playerSelection)
-    
-        if (winner === 1) {         // if returns 1, computer wins
-            computerWins += 1;
-        }
-        else if (winner === 0) {    // if returns 0, player wins
-            playerWins += 1;
-        }
-        else {
-            computerWins += 0;
-            playerWins += 0;
-        }
-    
-        console.log("Computer Wins",computerWins);
-        console.log("Player Wins",playerWins);
-            
-        if (computerWins === 3) {
-            console.log("THE COMPUTER WINS");
-        }
-        else if (playerWins === 3) {
-            console.log("THE PLAYER WINS")
-        }
-    
-    
-    });     
-
-}
-    
-
-
-game();
